8069e3be76dd962d820f878c292e32184763d360
Manisha11sep <er.manisha11sep@gmail.com>
2018-03-19 21:59:38 -0700

    Comleted 7 exercises

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      userInput : ''
+    }
+  }
+
+  updateValue(val)
+  {
+    this.setState({userInput : val})
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +26,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input className ="inputLine" onChange ={event =>this.updateValue(event.target.value)}/>
+        <h4> Entered String is :-  {this.state.userInput} </h4>
       </div>
     );
   }
